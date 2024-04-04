74121d023582a51de8a6fe44c2651f6ea51235d6
KaiSereni <155502072+KaiSereni@users.noreply.github.com>
2024-04-03 18:43:17 -0700

    onload not working

--- src/app/coding/page.tsx
+++ src/app/coding/page.tsx
@@ -5,26 +5,32 @@ import grid from "../../../public/image/grid.png"
 import clsx from "clsx";
 
 export default function Coding() {
-    interface numJson {
-        [index : number] : boolean
-    }
-    
+
     const [shownImage, setShownImage] = useState(0);
-    const [loadStatuses, setLoadStatuses] = useState<Number[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     const totalImages = 69;
 
+    function handleLoad() {
+        console.log("loaded")
+        setIsLoading(false);
+    }
+    try {
+        window.addEventListener("load", handleLoad);
+    }
+    catch(e){}
+
     useEffect(() => {
-        const handleScroll = () => {
+        function handleScroll() {
             let sh = window.scrollY;
-            setShownImage(Math.floor(sh/9))
+            setShownImage(Math.floor(sh/9));
         }
 
-        window.addEventListener("scroll", handleScroll)
+        window.addEventListener("scroll", handleScroll);
         return () => {
-            window.removeEventListener("scroll", handleScroll)
-        }
-    })
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [])
     
     const IDENodeList = () => {
         let r = []
@@ -39,16 +45,7 @@ export default function Coding() {
             const thisPath = `ide${thisName}.png`
 
             r.push(
-                <img src={thisPath} style={shownImage == i ? {display: "block"} : {display: "none"}} className="absolute max-h-[700px]" onLoad={() => {
-                    console.log(i + " loaded");
-                    let current = loadStatuses;
-                    current.push(i);
-                    setLoadStatuses(current);
-                }}
-                onError={() => {
-                    console.error("ERROR RENDERING IMAGE " + i + " IN ANIMATION");
-                }}
-                />
+                <img src={thisPath} style={shownImage == i ? {display: "block"} : {display: "none"}} className="absolute h-[700px]"/>
             )
         }
         return r;
@@ -57,7 +54,8 @@ export default function Coding() {
     return (
         <div className="block">
             <div className="block h-[700px]">
-                <div className="flex w-full h-full justify-center bg-gray-100">
+                <div className="flex w-full h-full justify-center bg-gray-400">
+                    <img src={grid.src} style={{mixBlendMode: "screen"}} className="absolute h-[700px]"/>
                     {
                         IDENodeList()
                     }
@@ -69,8 +67,8 @@ export default function Coding() {
             <div className="bg-white w-full h-[150vh]">
 
             </div>
-            <div className={clsx("absolute w-[100vw] h-[100vw] top-0 left-0 bg-blue-600 text-white text-center text-2xl", [loadStatuses.length >= totalImages ? "none" : "block"])}>
-                <p className="my-64">LOADING</p>
+            <div className={clsx("absolute w-[100vw] h-[100vh] top-0 left-0 bg-blue-800 text-2xl justify-center items-center text-center p-8 text-white", [isLoading ? "block" : "hidden"])}>
+                Loading...
             </div>
         </div>
     )
